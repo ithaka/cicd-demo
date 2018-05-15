65b459f6a8a683fd4dcea93b1a9bd6ce515f0759
Ryan Harrington <ryharrin@gmail.com>
2018-05-15 09:23:39 -0400

    fixed dockerfile

--- hello-world/src/app.js
+++ hello-world/src/app.js
@@ -2,6 +2,6 @@ const { addListener } = require('./hello.js')
 const express = require('express')
 const app = express()
 
-addListener(app, 'Hello world!')
+addListener(app, 'Hello World!')
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
